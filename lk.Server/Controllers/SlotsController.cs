ec662cee36fa96cb6ab1bc8b928f512e4d00ba9a
Александр <alexspb41@gmail.com>
2024-11-11 16:58:51 +0300

    Lk v0.0.0.15
    Добавлены запросы для получения смен конкретного пользователя.

--- lk.Server/Controllers/SlotsController.cs
+++ lk.Server/Controllers/SlotsController.cs
@@ -3,7 +3,6 @@ using lk.Server.DbContexts.RKNETDB.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using static lk.Server.Controllers.AuthorizationController;
 
 namespace lk.Server.Controllers
 {
@@ -26,9 +25,9 @@ namespace lk.Server.Controllers
             if (userPhone == null)
             {
                 return BadRequest(new { message = "user is null" });
-            }           
+            }
             List<JsonSlot> jsonSlots = new List<JsonSlot>();
-            List<WorkingSlots> workSlots = _rKNETDBContext.WorkingSlots.Include(c=>c.Locations)
+            List<WorkingSlots> workSlots = _rKNETDBContext.WorkingSlots.Include(c => c.Locations)
                                                                        .Include(c => c.JobTitles)
                                                                        .Where(c => c.Status == 0 && c.Begin > DateTime.Now.AddHours(1)).ToList();
             foreach (var workSlot in workSlots)
@@ -47,16 +46,69 @@ namespace lk.Server.Controllers
             return Ok(jsonSlots);
         }
 
-        [HttpGet("getuserssheets")]
-        [Authorize]
-        public IActionResult GetUsersSheets(string userPhone)
-        { 
-        
-        return Ok();
+        [HttpPatch("getoldusersheets")]
+        //[Authorize]
+        public IActionResult GetOldUserSheets([FromBody] GetUsersSheetsJson getUsersSheetsJson)
+        {
+            if (getUsersSheetsJson == null || getUsersSheetsJson.Phone == null || getUsersSheetsJson.Year == null || getUsersSheetsJson.Mounth == null)
+            {
+                return BadRequest(new { message = "unavailable data" });
+            }
+            try
+            {
+                DateTime begin = new DateTime(getUsersSheetsJson.Year.Value, getUsersSheetsJson.Mounth.Value, 1);
+                DateTime end = new DateTime(getUsersSheetsJson.Year.Value, getUsersSheetsJson.Mounth.Value + 1, 1);
+                List<WorkingSlots> workingSlots = _rKNETDBContext.WorkingSlots.Include(c => c.Personalities)
+                                                                              .Include(c => c.Locations)
+                                                                              .Include(c => c.JobTitles)
+                                                                              .Where(c => c.Personalities.Phone == getUsersSheetsJson.Phone && c.Status != 1 && c.Begin >= begin && c.End < end)
+                                                                              .ToList();
+                List<TimeSheets> timeSheets = _rKNETDBContext.TimeSheets.Include(c => c.Personalities)
+                                                                        .Include(c => c.Location)
+                                                                        .Include(c => c.JobTitle)
+                                                                        .Where(c => c.Personalities.Phone == getUsersSheetsJson.Phone && c.Begin >= begin && c.End < end && c.Begin < DateTime.Now)
+                                                                        .ToList();
+                return Ok(CompleeteGetUsersSheetsResponse(workingSlots, timeSheets));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+
         }
 
-        [HttpPut("takesheet")]
+        [HttpGet("getfutureusersheets")]
        // [Authorize]
+        public IActionResult GetFutureUserSheets(string phone)
+        {
+            if (phone == null)
+            {
+                return BadRequest(new { message = "unavailable data" });
+            }
+            try
+            {
+                List<WorkingSlots> workingSlots = _rKNETDBContext.WorkingSlots.Include(c => c.Personalities)
+                                                                          .Include(c => c.Locations)
+                                                                          .Include(c => c.JobTitles)
+                                                                          .Where(c => c.Personalities.Phone == phone && (c.Status == 1))
+                                                                          .ToList();
+                List<TimeSheets> timeSheets = _rKNETDBContext.TimeSheets.Include(c => c.Personalities)
+                                                                        .Include(c => c.Location)
+                                                                        .Include(c => c.JobTitle)
+                                                                        .Where(c => c.Personalities.Phone == phone && c.Begin > DateTime.Now)
+                                                                        .ToList();
+                return Ok(CompleeteGetUsersSheetsResponse(workingSlots, timeSheets));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+
+
+        [HttpPut("takesheet")]
+        [Authorize]
         public IActionResult TakeSheet([FromBody] TakeSheetJson takeSheetJson)
         {
             if (takeSheetJson == null || takeSheetJson.Id == null || takeSheetJson.Phone == null)
@@ -118,6 +170,55 @@ namespace lk.Server.Controllers
         {
             public int? Id { get; set; }
             public string? Phone { get; set; }
+        }
+
+        public class GetUsersSheetsJson
+        {
+            public string Phone { get; set; }
+            public int? Year { get; set; }
+            public int? Mounth { get; set; }
+        }
+
+        public class GetUsersSheetsJsonResponse
+        {
+            public int? Id { get; set; }
+            public Guid? Guid { get; set; }
+            public string LocationName { get; set; }
+            public string JobTitleName { get; set; }
+            public string Date { get; set; }
+            public string BeginTime { get; set; }
+            public string EndTime { get; set; }
+            public int? Status { get; set; }
+
+        }
+
+        private List<GetUsersSheetsJsonResponse> CompleeteGetUsersSheetsResponse(List<WorkingSlots> workingSlots, List<TimeSheets> timeSheets)
+        {
+            List<GetUsersSheetsJsonResponse> jsonResponse = new();
+            foreach (var item in workingSlots)
+            {
+                GetUsersSheetsJsonResponse usersSheet = new();
+                usersSheet.Id = item.Id;
+                usersSheet.LocationName = item.Locations.Name;
+                usersSheet.JobTitleName = item.JobTitles.Name;
+                usersSheet.Date = item.Begin.ToString("dd.MM.yyyy");
+                usersSheet.BeginTime = item.Begin.ToString("HH.mm");
+                usersSheet.EndTime = item.Begin.ToString("HH.mm");
+                usersSheet.Status = item.Status;
+                jsonResponse.Add(usersSheet);
+            }
+            foreach (var item in timeSheets)
+            {
+                GetUsersSheetsJsonResponse usersSheet = new();
+                usersSheet.Guid = item.Guid;
+                usersSheet.LocationName = item.Location.Name;
+                usersSheet.JobTitleName = item.JobTitle.Name;
+                usersSheet.Date = item.Begin.ToString("dd.MM.yyyy");
+                usersSheet.BeginTime = item.Begin.ToString("HH.mm");
+                usersSheet.EndTime = item.Begin.ToString("HH.mm");
+                jsonResponse.Add(usersSheet);
+            }
+            return jsonResponse;
         }
     }
 }
